e2ff4e35b0cb1e9e7ac27eccd773e24d37c09b1c
Luis Leopardi <supernano1998@gmail.com>
2020-05-28 06:50:50 -0400

    Updates

---
+++ precache-manifest.b19e216f24a9e8e66468e784faa46da5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d784778884cfd09ac13bfbfbfa4d42ab",
+    "url": "/Portafolio/index.html"
+  },
+  {
+    "revision": "4b4532c578cb3437e9dd",
+    "url": "/Portafolio/static/css/main.4a2d1a42.chunk.css"
+  },
+  {
+    "revision": "b3db7b7ff6dd88c338aa",
+    "url": "/Portafolio/static/js/2.5b2d6bdf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portafolio/static/js/2.5b2d6bdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b4532c578cb3437e9dd",
+    "url": "/Portafolio/static/js/main.77114a8e.chunk.js"
+  },
+  {
+    "revision": "756d58d19a6f5cc57765",
+    "url": "/Portafolio/static/js/runtime-main.58444507.js"
+  },
+  {
+    "revision": "d0588c843014030c542dc232667c0a32",
+    "url": "/Portafolio/static/media/instagram.d0588c84.svg"
+  },
+  {
+    "revision": "5a7b7a148e64562f88af7df0cb548a2d",
+    "url": "/Portafolio/static/media/karate.5a7b7a14.png"
+  },
+  {
+    "revision": "715db17a829321b44bfe061c1b95cd62",
+    "url": "/Portafolio/static/media/linkedin.715db17a.svg"
+  },
+  {
+    "revision": "c49b8133e9b11692959fce2b53846311",
+    "url": "/Portafolio/static/media/me.c49b8133.png"
+  }
+]);
