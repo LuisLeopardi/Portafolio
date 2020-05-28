5fd045a3053a50495918ea7d14b4deeecd84743a
Luis Leopardi <supernano1998@gmail.com>
2020-05-28 12:44:49 -0400

    commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import {NavLink} from 'react-router-dom';
+import {BrowserRouter as Router, NavLink} from 'react-router-dom';
 
 export const Navbar = () => {
 return (
     <>
+    <Router>
     <h1 className='logo'> Websites <br/> <i>&amp; apps </i>  </h1>
     <nav className='navbar'>     
         <NavLink to="/"> Projects </NavLink>
         <NavLink to="/about"> About me </NavLink>   
         <p >Order <i>(comming soon)</i>  </p>
     </nav>
+    </Router>
     </>
 )}
