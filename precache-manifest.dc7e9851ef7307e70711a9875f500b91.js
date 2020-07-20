197088a6b7b1a5c9a04ba74b42bd20548f0b892b
Luis Leopardi <supernano1998@gmail.com>
2020-07-20 10:51:18 -0400

    Updates

---
+++ precache-manifest.dc7e9851ef7307e70711a9875f500b91.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c20115b053520f46a6606053198eba60",
+    "url": "/Portafolio/index.html"
+  },
+  {
+    "revision": "e6fabb78ff6942427da4",
+    "url": "/Portafolio/static/css/main.95e43334.chunk.css"
+  },
+  {
+    "revision": "6f55eae9f4cd710af74b",
+    "url": "/Portafolio/static/js/2.7359d8f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portafolio/static/js/2.7359d8f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6fabb78ff6942427da4",
+    "url": "/Portafolio/static/js/main.2d302238.chunk.js"
+  },
+  {
+    "revision": "756d58d19a6f5cc57765",
+    "url": "/Portafolio/static/js/runtime-main.58444507.js"
+  },
+  {
+    "revision": "077ec8d05b328993208b399d0a53cece",
+    "url": "/Portafolio/static/media/chatApp.077ec8d0.PNG"
+  },
+  {
+    "revision": "6339a0b9ce359c76d75944d06e010ffb",
+    "url": "/Portafolio/static/media/instagram.6339a0b9.svg"
+  },
+  {
+    "revision": "5a7b7a148e64562f88af7df0cb548a2d",
+    "url": "/Portafolio/static/media/karate.5a7b7a14.png"
+  },
+  {
+    "revision": "9a1ba7dd0cb25eef918d86bd90640420",
+    "url": "/Portafolio/static/media/linkedin.9a1ba7dd.svg"
+  },
+  {
+    "revision": "c49b8133e9b11692959fce2b53846311",
+    "url": "/Portafolio/static/media/me.c49b8133.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portafolio/precache-manifest.6a1caf87f28baf8becc049d68890cb50.js"
+  "/Portafolio/precache-manifest.dc7e9851ef7307e70711a9875f500b91.js"
 );
 
 self.addEventListener('message', (event) => {
